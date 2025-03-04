e6bab2d9ea62c31540091539ea2fb6f0579acf4f
benrise_x <benrise_x@mail.ru>
2025-03-04 12:44:11 +0300

    Minor fix

--- app/entities/terms/ui/Terms.tsx
+++ app/entities/terms/ui/Terms.tsx
@@ -7,6 +7,6 @@ export function Terms() {
     const terms_link = process.env.NEXT_TERMS_LINK
 
     return (
-        <Paragraph type='secondary' className='text-[12px]! mb-0! text-center!'>By continuing, you agree to the <Button type='link' className="text-[12px]!" href={terms_link}>AI Love Photo Terms</Button> of Use and Privacy Policy</Paragraph>
+        <Paragraph type='secondary' className='text-[12px]! mb-0! text-center!'>By continuing, you agree to the <Button type='link' className="text-[12px]! h-fit!" href={terms_link}>AI Love Photo Terms</Button> of Use and Privacy Policy</Paragraph>
     )
 }
