4ec32213fd117d341308b1a59a41cde443f6357b
benrise_x <benrise_x@mail.ru>
2025-03-16 01:02:13 +0300

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/app/tailwind.css';
 import '@/app/style.scss';
 
-import type { Metadata } from "next";
+import { type Metadata } from "next";
 import { ConfigProvider } from 'antd';
 import { Toaster } from 'sonner';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
@@ -9,10 +9,21 @@ import { AntdRegistry } from '@ant-design/nextjs-registry';
 import { MainProvider } from '@/app/providers';
 import { theme } from '@/app/theme.config';
 
-
 export const metadata: Metadata = {
-  title: "AI Love Photo",
+  title: 'AI Love Photo',
   description: "AI Love Photo - your personal photoclone",
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicon/favicon.svg', type: 'image/svg+xml' },
+    ],
+    shortcut: '/favicon/favicon.ico',
+    apple: '/favicon/apple-touch-icon.png',
+  },
+  manifest: '/favicon/site.webmanifest',
+  appleWebApp: {
+    title: 'AI Love Photo',
+  },
 };
  
 export default function RootLayout({
